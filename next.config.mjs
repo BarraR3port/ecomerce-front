73bd267018db603680f8af640b285a2b79a5d845
BarraR3port <barrar3port@gmail.com>
2024-04-25 16:04:18 -0400

    feat: Estado inicial de la pagina

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import million from "million/compiler";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	transpilePackages: ["lucide-react"],
+	swcMinify: true,
+	distDir: "dist",
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "utfs.io",
+				port: ""
+			}
+		]
+	}
+};
+
+const millionConfig = {
+	auto: true,
+	rsc: true
+};
+
+export default million.next(nextConfig, millionConfig);
